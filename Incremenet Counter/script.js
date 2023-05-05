bf83bfdff4b905d05bea479ae9625fe16800c5a1
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-05 09:52:24 +0100

    Add files via upload

---
+++ Incremenet Counter/script.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    // 200
+    const c = +counter.innerText;
+    // 0
+    const increment = target / 200;
+    // 1
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
