e72a11177136b79d8cbe1a649b27869a190b0dfd
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-07-20 08:33:08 +0100

    Update script.js

--- Choices Picker/script.js
+++ Choices Picker/script.js
@@ -4,7 +4,7 @@ const textarea = document.getElementById("textarea");
 textarea.focus();
 
 textarea.addEventListener ("keyup", (e) => {
-  createTags(e.target.value);
+  createTags(e.target.value); 
 
   if (e.key === "Enter") {
     setTimeout(() => {
