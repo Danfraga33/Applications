db8533d7bc6607acdc05fca8d7dbf1bdb06e8cf5
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-09-07 07:52:07 +0100

    Update script.js

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -4,7 +4,7 @@ const loadText = document.querySelector(".loading-text");
 const bg = document.querySelector(".bg");
 
 let load = 0;  
-
+ 
 let int = setInterval(blurring, 20);
 
 function blurring() {
